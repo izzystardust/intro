ceaef20b6db5864df1dca8d8e1c3d6ca2d710c9f
Isabelle Miller <izzy@stardust.fm>
2014-03-19 14:59:45 -0400

    How to print. The first lesson: printing.

---
+++ 1/print.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the pace calculator\n");
+	fmt.Println("Please enter distances in miles and times in the format\nhours:minutes:seconds, as in 0:32:27.\n")
+}
